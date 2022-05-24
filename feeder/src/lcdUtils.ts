2614ee6e2f52634fa737f75ed991b2697529e349
jjangg96 <jjangg96@gmail.com>
2022-05-24 12:20:44 +0900

    Don't use binance.

--- feeder/src/lcdUtils.ts
+++ feeder/src/lcdUtils.ts
@@ -10,15 +10,15 @@ export async function getPricesFromLCD(client: LCDClient): Promise<Price[]> {
   console.info(`timestamp: ${new Date().toUTCString()}`)
   console.info(`getting price data from lcd`)
 
-  const lastPrice = await lastBinancePrice()
+  // const lastPrice = await lastBinancePrice()
   return client.oracle.exchangeRates().then((results) => {
     const usd = results.get(Denom.USD)?.amount || new Dec(0)
     console.log('oracle from lcd', results)
-    console.log(`binance ${lastPrice}, oracle ${usd}`)
-
-    if (usd.minus(new Dec(lastPrice)).abs().gt(2)) {
-      throw new Error(`USD price invalid, binance ${lastPrice}, oracle ${usd}`)
-    }
+    // console.log(`binance ${lastPrice}, oracle ${usd}`)
+    //
+    // if (usd.minus(new Dec(lastPrice)).abs().gt(2)) {
+    //   throw new Error(`USD price invalid, binance ${lastPrice}, oracle ${usd}`)
+    // }
 
     return results.toArray().map((coin) => {
       return {
@@ -29,19 +29,19 @@ export async function getPricesFromLCD(client: LCDClient): Promise<Price[]> {
   })
 }
 
-const lastBinancePrice = async (): Promise<number> => {
-  const params = { symbol: 'LUNAUSDT', interval: '1m', limit: 1 }
-  const query = Object.keys(params)
-    .map((key) => `${key}=${params[key]}`)
-    .join('&')
-
-  return await axios
-    .get(`https://api.binance.com/api/v3/klines?${query}`)
-    .then((res) => {
-      return parseFloat(res.data[0][4])
-    })
-    .catch((e) => {
-      console.log(`lastPrice(): invalid api response:`, e ? JSON.stringify(e) : 'empty')
-      throw new Error(`lastPrice(): invalid response`)
-    })
-}
+// const lastBinancePrice = async (): Promise<number> => {
+//   const params = { symbol: 'LUNAUSDT', interval: '1m', limit: 1 }
+//   const query = Object.keys(params)
+//     .map((key) => `${key}=${params[key]}`)
+//     .join('&')
+//
+//   return await axios
+//     .get(`https://api.binance.com/api/v3/klines?${query}`)
+//     .then((res) => {
+//       return parseFloat(res.data[0][4])
+//     })
+//     .catch((e) => {
+//       console.log(`lastPrice(): invalid api response:`, e ? JSON.stringify(e) : 'empty')
+//       throw new Error(`lastPrice(): invalid response`)
+//     })
+// }
